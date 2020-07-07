b55919fe296c70b70d912f53e520faf6ee7898a3
tsol_kimura <tsol_kimura@aol.com>
2020-07-07 16:26:15 +0900

    1

---
+++ 新しいフォルダー/Cj_1_1.java
@@ -0,0 +1,12 @@
+package pr_1_1;
+
+public class Cj_1_1 {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+//		for (int i = 0; i < args.length; i++) {
+//
+//		}
+	}
+
+}
